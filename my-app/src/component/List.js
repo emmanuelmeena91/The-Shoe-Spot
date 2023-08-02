62235ab488ccd4999db849eaab7127438cc6a03e
emmanuelmeena91 <emmanuel.meena@moringaschool.com>
2023-08-02 21:32:49 +0300

    complete

--- my-app/src/component/List.js
+++ my-app/src/component/List.js
@@ -8,7 +8,7 @@ const List = () => {
     
       <h1 className="text-7xl font-bold text-white shadow-silver bg-black p-4 ml-2 mr-2 mt-2 mb-2">The-Shoe-Spot</h1>
   
-      <Nav/>
+      <Nav/>  
 
       <Sneaker/>
 
